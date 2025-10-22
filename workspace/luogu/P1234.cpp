534fc594b0fb7d0188ecb36acc24d468985f1bcb
_little_duck_ <little_duck0529@outlook.com>
2025-10-22 18:48:29 +0800

    code change 8

--- workspace/luogu/P1234.cpp
+++ workspace/luogu/P1234.cpp
@@ -15,13 +15,12 @@
 using namespace std;
 
 const int N = 1010;
+const int dx[] = {0, 0, -1, 1};
+const int dy[] = {-1, 1, 0, 0};
 
-int n, m;
+int n, m, vis[N][N];
 char c[N][N];
-
-void dfs(int x, int y){
-  
-}
+string comp = "hehe";
 
 int main(){
   ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
@@ -32,6 +31,23 @@ int main(){
       cin >> c[i][j];
     }
   }
-  dfs(1, 1);
+  int ans = 0;
+  for(int i = 1; i <= n; i++){  
+    for(int j = 1; j <= m; j++){
+      for(int dir = 0; dir < 4; dir++){
+        int x = i, y = j, k = 0;
+        for(; k < 4; k++){
+          if(x < 0 || y < 0 || x > n || y > m || comp[k] != c[x][y]){
+            break;
+          }
+          x += dx[dir], y += dy[dir];
+        }
+        if(k == 4){
+          ans++;
+        }
+      }
+    }
+  }
+  cout << ans;
   return 0;
 }
