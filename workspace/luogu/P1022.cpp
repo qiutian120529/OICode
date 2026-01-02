0d7f87ba9043ed7eec8ed2c532f85a424d0bc940
_little_duck_ <little_duck0529@outlook.com>
2026-01-02 18:37:49 +0800

    code change 20

--- workspace/luogu/P1022.cpp
+++ workspace/luogu/P1022.cpp
@@ -8,20 +8,27 @@ const int N = 1e5 + 10;
 char c, x;
 
 int num, side = 1, sum, p, f = 1; // px = 2, x= 2 / p
+bool have;
 
 int main(){
   ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
   while(cin >> c){
     if(isdigit(c)){
       num = num * 10 + (c - '0');
+      have = 1;
     }
     else if('a' <= c && c <= 'z'){
-      p += num * f * side;
-      num = 0, f = 1;
+      x = c;
+      if(have)
+        p += num * f * side;
+      else
+        p += f * side;
+       num = 0, f = 1;
+      have = 0;
     }
     else{
       sum += num * f * side;
-      num = 0, f = 1;
+      num = 0, f = 1, have = 0;
       if(c == '='){
         side = -1;
       }
@@ -34,6 +41,6 @@ int main(){
     }
   }
   sum += num * f * side;
-  cout << x << "=" << fixed << setprecision(3) << sum / p;
+  cout << x << "=" << fixed << setprecision(3) << (-sum * 1.0 / p == -0.0 ? 0 : -sum * 1.0 / p);
   return 0;
 }
